ce3352c9dcda1d859b97ec851ec7f7c3715ad66d
Moritz <moritz.mueller2@tu-dresden.de>
2023-05-24 12:25:50 +0200

    fix quote style

--- mysite/expertise/models.py
+++ mysite/expertise/models.py
@@ -6,44 +6,44 @@ class ResearchInterest(DjangoNode):
     pk = UniqueIdProperty()
 
     class Meta:
-        app_label = 'expertise'
+        app_label = "expertise"
 
 class Institute(DjangoNode):
     name = StringProperty(unique_index=True, required=True)
     pk = UniqueIdProperty()
 
     class Meta:
-        app_label = 'expertise'
+        app_label = "expertise"
 
 class Faculty(DjangoNode):
     name = StringProperty(unique_index=True, required=True)
     pk = UniqueIdProperty()
 
     class Meta:
-        app_label = 'expertise'
-        verbose_name_plural = 'faculties'
+        app_label = "expertise"
+        verbose_name_plural = "faculties"
 
 class Department(DjangoNode):
     name = StringProperty(unique_index=True, required=True)
     pk = UniqueIdProperty()
 
     class Meta:
-        app_label = 'expertise'
+        app_label = "expertise"
 
 class Expertise(DjangoNode):
     name = StringProperty(unique_index=True, required=True)
     pk = UniqueIdProperty()
 
     class Meta:
-        app_label = 'expertise'
-        verbose_name_plural = 'expertise'
+        app_label = "expertise"
+        verbose_name_plural = "expertise"
 
 class Role(DjangoNode):
     name = StringProperty(unique_index=True, required=True)
     pk = UniqueIdProperty()
 
     class Meta:
-        app_label = 'expertise'
+        app_label = "expertise"
 
 class Person(DjangoNode):
     def graph_data(self):
@@ -108,14 +108,14 @@ class Person(DjangoNode):
     comment = StringProperty()
     pk = UniqueIdProperty()
 
-    interests = RelationshipTo(ResearchInterest, 'HAS')
-    institutes = RelationshipTo(Institute, 'MEMBER_OF')
-    faculties = RelationshipTo(Faculty, 'MEMBER_OF')
-    departments = RelationshipTo(Department, 'MEMBER_OF')
-    roles = RelationshipTo(Role, 'HAS')
-    offered_expertise = RelationshipTo(Expertise, 'OFFERS')
-    wanted_expertise = RelationshipTo(Expertise, 'WANTS')
-    advisors = RelationshipTo('Person', 'ADVISED_BY')
+    interests = RelationshipTo(ResearchInterest, "HAS")
+    institutes = RelationshipTo(Institute, "MEMBER_OF")
+    faculties = RelationshipTo(Faculty, "MEMBER_OF")
+    departments = RelationshipTo(Department, "MEMBER_OF")
+    roles = RelationshipTo(Role, "HAS")
+    offered_expertise = RelationshipTo(Expertise, "OFFERS")
+    wanted_expertise = RelationshipTo(Expertise, "WANTS")
+    advisors = RelationshipTo("Person", "ADVISED_BY")
 
     class Meta:
-        app_label = 'expertise'
+        app_label = "expertise"
