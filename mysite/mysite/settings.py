e989da4b571d58a63399f88278fd9dc435579654
Moritz <moritz.mueller2@tu-dresden.de>
2023-06-22 15:58:07 +0200

    default allowed hosts

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', SERVER IP]
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', 'SERVER IP']
 
 # Application definition
 
