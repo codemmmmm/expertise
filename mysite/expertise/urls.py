e9339036697ab8203f27191387586f198ee4c222
Moritz <moritz.mueller2@tu-dresden.de>
2023-11-01 16:14:05 +0100

    about page

--- mysite/expertise/urls.py
+++ mysite/expertise/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -10,4 +11,5 @@ urlpatterns = [
     path('graph', views.graph_api, name='graph'),
     path('approve', views.approve, name='approve'),
     path('shorten', views.shorten, name='share'),
+    path('about', TemplateView.as_view(template_name='expertise/about.html'), name='about'),
 ]
